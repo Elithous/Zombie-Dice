6498f0f4ad297a25ddfbaf5568c1de2d2bf7cba2
Joe <jtillotson@student.neumont.edu>
2017-11-01 14:23:16 -0600

    The player class

--- src/team/Seal/Player.java
+++ src/team/Seal/Player.java
@@ -4,11 +4,17 @@ public class Player {
 
     private String Name;
     private int points;
+    private int brains;
+    private int shots;
+
 
     public Player(String name){
 
         this.Name = Name;
         this.points = 0;
+        this.brains=0;
+
+
     }
 
     public String getName() {
@@ -23,4 +29,35 @@ public class Player {
         this.points = points;
     }
 
+    private void setBrains(int brains){
+        this.brains = brains;
+    }
+    public int getbrains(){
+        return brains;
+    }
+
+    private void setShots(int shots){
+        this.shots=shots;
+    }
+
+    public int getShots(){
+        return shots;
+
+    }
+    public String winner(){
+        if(this.brains == 13)
+            return "you win";
+        else {
+            return "";
+        }
+
+    }
 }
+/* define a player.
+public void play game
+sout how many players
+scanner to pick how many
+put it into palyer array then use for loop
+ask for name
+
+ */
